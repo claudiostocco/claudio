3f85b6e962fb8156f3afd278b96039f3b7a605f3
claudiostocco <claudiostocco@gmail.com>
2023-07-06 11:31:51 -0300

    Download ibexpert.rar

--- src/pages/files.tsx
+++ src/pages/files.tsx
@@ -2,6 +2,7 @@ export default function Files() {
     return (
         <ul>
             <li><a href="/files/instalar.tmp">instalar.rar</a></li>
+            <li><a href="/files/ibexpert.tmp">ibexpert.rar</a></li>
         </ul>
     )
 }
