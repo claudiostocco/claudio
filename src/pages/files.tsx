99b2d278354981f6b13a680668b13a697de23c12
Claudio Marcio Stocco <claudiostocco@gmail.com>
2024-04-03 16:10:23 -0300

    Update files.tsx

--- src/pages/files.tsx
+++ src/pages/files.tsx
@@ -3,6 +3,7 @@ export default function Files() {
         <ul>
             <li><a href="/files/instalar.tmp">instalar.rar</a></li>
             <li><a href="/files/ibexpert.tmp">ibexpert.rar</a></li>
+            <li><a href="https://drive.google.com/file/d/10HYnagElWHFEa6fpk5zEgJxemr1WnuLP/view?usp=sharing">gt-at.rar</a></li>
         </ul>
     )
 }
